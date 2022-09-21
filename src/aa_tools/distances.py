48771a3087f0f4ca0a4eaf43050a331cef42ca47
Eloy Chang <=>
2022-09-21 12:49:32 -0300

    Quitar log de haversine

--- src/aa_tools/distances.py
+++ src/aa_tools/distances.py
@@ -1,12 +1,11 @@
 
 import math
 
-def haversine(lon1, lat1, lon2, lat2, log, r = 6371000):
+def haversine(lon1, lat1, lon2, lat2, r = 6371000):
     """
     Calculate the great circle distance in kilometers between two points
     on the earth (specified in decimal degrees)
     """
-    log = log.start_function("haversine")
 
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
@@ -16,7 +15,5 @@ def haversine(lon1, lat1, lon2, lat2, log, r = 6371000):
     dlat = lat2 - lat1
     a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
     c = 2 * math.asin(math.sqrt(a))
-
-    log.close()
 
     return c * r
