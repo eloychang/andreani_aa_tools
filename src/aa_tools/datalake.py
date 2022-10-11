8a47ada8ac6f4bd95f7b9654bffc936ad5c69e19
Eloy Chang <echang@andreani.com>
2022-10-11 14:51:19 -0300

    Agregar upload de json

--- src/aa_tools/datalake.py
+++ src/aa_tools/datalake.py
@@ -1,35 +1,45 @@
 
-from azure.storage.blob import BlobServiceClient
+from azure.storage.filedatalake import DataLakeServiceClient
 import pandas as pd
 from io import BytesIO
+import pickle
 
 class datalake():
 
-    def __init__(self, dl_url, dl_key):
+    def __init__(self, credentials, file_system = "datalake"):
+        with open(credentials, "rb") as file:
+            storage_account_name, storage_account_key = pickle.load(file)
 
-        self._client = BlobServiceClient(account_url = dl_url, credential = dl_key)
+        # Defino la conexión al datalake
+        service_client = DataLakeServiceClient(
+                account_url="{}://{}.dfs.core.windows.net".format("https", storage_account_name),
+                credential=storage_account_key)
+        self._client = service_client.get_file_system_client(file_system=file_system)
 
         self._import_settings = {
             "parquet" : pd.read_parquet,
             "csv" : pd.read_csv
         }
 
-    def import_file(self, file_system, path, format):
-        file_client = self._client.get_blob_client(file_system, path, snapshot=None)
 
+    def import_file(self, path, filename, read_format):
+        directory_client = self._client.get_directory_client(path)
+        file_client = directory_client.get_file_client(filename)
         # descargo los datos
-        download = file_client.download_blob()
+        download = file_client.download_file()
         downloaded_bytes = download.readall()
 
-        return self._import_settings[format](BytesIO(downloaded_bytes))
+        return self._import_settings[read_format](BytesIO(downloaded_bytes))
 
 
-    def upload_file(self, data, file_system, path, format, index = False):
-        file_client = client.get_blob_client(file_system, path, snapshot=None)
+    def upload_file(self, data, path, filename, write_format):
+        directory_client = self._client.get_directory_client(path)
 
-        if format == "parquet":
-            file_contents = data.to_parquet(index=index).encode()
-        elif format == "csv":
-            file_contents = data.to_csv(index=index).encode()
-
-        file_client.upload_blob(file_contents)
+        file_client = directory_client.create_file(filename)
+        if write_format == "parquet":
+            file_contents = data.to_parquet(index=False).encode()
+        elif write_format == "csv":
+            file_contents = data.to_csv(index=False).encode()
+        elif write_format == "json":
+            file_contents = data
+        file_client.upload_data(file_contents, overwrite=True)
